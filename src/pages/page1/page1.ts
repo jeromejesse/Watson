5e550b8ad17834eb016693d8c6e0b8ff57b9d508
JEJ <jej@foyer.lu>
2017-03-05 22:59:04 +0100

    speech recognition

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ChangeDetectorRef} from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-import { Keyboard } from 'ionic-native';
+import { Keyboard, SpeechRecognition } from 'ionic-native';
 
 @Component({
   selector: 'page-page1',
@@ -10,14 +10,16 @@ import { Keyboard } from 'ionic-native';
 export class Page1 {
   messages: Array<{title: string, message: string, float:string}>;
   message: string = '';
+  record: Boolean = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private ref: ChangeDetectorRef) {
     this.messages = [];
     this.messages.push({
       title: 'Watson',
       message:'Bonjour je suis Watson, votre assistant personnel en assurance',
       float: 'left'
     })
+    this.ref = ref;
   }
 
   outsideKeyboard(event) {
@@ -26,17 +28,56 @@ export class Page1 {
 
 
   postMessage(event, message) {
-    this.messages.push({
-      title: 'Vous',
-      message: message,
-      float: 'right'
-    });
+    this.addMessage(message);
     this.message = '';
   }
 
   listenMessage(event) {
     Keyboard.close();
-    this.message = '';
+    let options = {
+      language: "fr-FR",
+      matches: 1,
+      showPartial: false // iOS only
+    };
+    this.record = !this.record;
+    if(this.record){
+      SpeechRecognition.hasPermission()
+        .then((hasPermission) => {
+          if (hasPermission){
+            this.startListening(options)
+          } else {
+            SpeechRecognition.requestPermission()
+              .then(
+                () => console.log('Granted'),
+                () => console.log('Denied')
+              )
+          }
+        });
+    } else {
+      SpeechRecognition.stopListening();
+    }
+
+  }
+
+  startListening(options) {
+    SpeechRecognition.startListening(options)
+      .subscribe(
+        (matches) => {
+          console.log(matches);
+          //this.message = matches[0];
+          this.addMessage(matches[0]);
+          this.ref.detectChanges();
+        },
+        (onerror) => console.log('error:', onerror)
+      );
+  }
+
+  addMessage(message){
+    this.messages.push({
+      title: 'Vous',
+      message: message,
+      float: 'right'
+    });
   }
 
 }
